a5dfda52994edf0481f1fbffdc7ce74a6b1d509f
Leland Baker <42isslay@gmail.com>
2026-04-03 17:49:01 -0400

    added 5 buttons

---
+++ Assets/Scripts/TheBigUI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TheBigUI : MonoBehaviour
+{
+    public void TheBigMSG()
+    {
+        Debug.Log("The Big MSG");
+    }
+    
+    public void TheBigGinger()
+    {
+        Debug.Log("The Big Ginger");
+    }
+    
+    public void TheBigGarlic()
+    {
+        Debug.Log("The Big Garlic");
+    }
+    
+    public void TheBigZest()
+    {
+        Debug.Log("The Big Zest");
+    }
+    
+    public void TheBigCumin()
+    {
+            Debug.Log("The Big Cumin");
+    }
+}
