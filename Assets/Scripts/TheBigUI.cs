99c22165708d6853eb847615fef667becc201874
Leland Baker <42isslay@gmail.com>
2026-04-12 16:30:32 -0400

    list of food

--- Assets/Scripts/TheBigUI.cs
+++ Assets/Scripts/TheBigUI.cs
@@ -1,29 +1,95 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+
 public class TheBigUI : MonoBehaviour
 {
+
+    private bool[] theBigTable = new bool[5];
+    public TextMeshProUGUI theBigText;
+    public Button theBigGinger;
+    public Button theBigMSG;
+    public Button theBigGarlic;
+    public Button theBigZest;
+    public Button theBigCumin;
+    
     public void TheBigMSG()
     {
-        Debug.Log("The Big MSG");
+        if (TheBigCheck() == true)
+        {
+            theBigTable[0] = true;
+            Debug.Log("The Big MSG");
+            theBigText.text += " - MSG                          ";
+            theBigMSG.interactable = false;
+        }
     }
     
     public void TheBigGinger()
     {
-        Debug.Log("The Big Ginger");
+        if (TheBigCheck() == true)
+        {
+            theBigTable[1] = true;
+            Debug.Log("The Big Ginger");
+            theBigText.text += " - Ginger                       ";
+            theBigGinger.interactable = false;
+        }
     }
     
     public void TheBigGarlic()
     {
-        Debug.Log("The Big Garlic");
+        if (TheBigCheck() == true)
+        {
+            theBigTable[2] = true;
+            Debug.Log("The Big Garlic");
+            theBigText.text += " - Garlic                       ";
+            theBigGarlic.interactable = false;
+        }
     }
     
     public void TheBigZest()
     {
-        Debug.Log("The Big Zest");
+        if (TheBigCheck() == true)
+        {
+            theBigTable[3] = true;
+            Debug.Log("The Big Zest");
+            theBigText.text += " - Zest                         ";
+            theBigZest.interactable = false;
+        }
     }
     
     public void TheBigCumin()
     {
+        if (TheBigCheck() == true)
+        {
+            theBigTable[4] = true;
             Debug.Log("The Big Cumin");
+            theBigText.text += " - Cumin                        ";
+            theBigCumin.interactable = false;
+        }
+    }
+
+    public bool TheBigCheck()
+    {
+        int theBigElements = 0;
+        for (int i = 0; i < theBigTable.Length; i++)
+        {
+
+            if (theBigTable[i])
+            {
+                theBigElements++;
+            }
+
+        }
+        if (theBigElements >= 3)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
 }
